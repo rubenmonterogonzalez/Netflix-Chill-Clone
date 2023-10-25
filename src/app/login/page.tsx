94f77f6588448d24d2ff3714d5f340fc8aec5c3e
rubenmonterogonzalez <rubenmontero@rocketmail.com>
2023-10-25 16:43:36 +0200

    fix form

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,23 +1,63 @@
 import Image from "next/image";
 import Form from "~/components/auth-components/form";
 import Link from "next/link";
+import { css } from "~/styled-system/css";
 
 export default function Login() {
   return (
-    <div>
-      <div>
-        <div>
+    <div
+      className={css({
+        display: 'flex',
+        h: 'screen',
+        w: 'screen',
+        alignItems: 'center',
+        justifyContent: 'center',
+        bgColor: 'gray.50',
+      })}
+    >
+      <div
+        className={css({
+          zIndex: '10',
+          w: 'full',
+          maxW: 'md',
+          overflow: 'hidden',
+          rounded: '2xl',
+          borderWidth: '1px',
+          borderColor: 'gray.100',
+          shadow: 'xl',
+        })}
+      >
+        <div
+          className={css({
+            display: 'flex',
+            flexDir: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+            mt: '3',
+            mb: '3',
+            borderBottomWidth: '1px',
+            borderColor: 'gray.200',
+            bgColor: 'white',
+            pl: '4',
+            pr: '4',
+            pt: '8',
+            pb: '6',
+            textAlign: 'center',
+            sm: { pl: '16', pr: '16' },
+          })}
+        >
           <Link href="/">
             <Image
               src="/logo.png"
               priority
               alt="Logo"
+              className={css({ h: '10', w: '10', rounded: 'full' })}
               width={20}
               height={20}
             />
           </Link>
-          <h3>Sign In</h3>
-          <p>
+          <h3 className={css({ fontSize: 'xl', lineHeight: 'xl', fontWeight: 'semibold' })}>Sign In</h3>
+          <p className={css({ fontSize: 'sm', lineHeight: 'sm', color: 'gray.500' })}>
             Use your email and password to sign in
           </p>
         </div>
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,25 +1,64 @@
 import Image from "next/image";
 import Form from "~/components/auth-components/form";
 import Link from "next/link";
+import { css } from "~/styled-system/css";
 
 export default function Login() {
   return (
-    <div className="flex h-screen w-screen items-center justify-center bg-gray-50">
-      <div className="z-10 w-full max-w-md overflow-hidden rounded-2xl border border-gray-100 shadow-xl">
-        <div className="flex flex-col items-center justify-center space-y-3 border-b border-gray-200 bg-white px-4 py-6 pt-8 text-center sm:px-16">
+    <div
+      className={css({
+        display: 'flex',
+        h: 'screen',
+        w: 'screen',
+        alignItems: 'center',
+        justifyContent: 'center',
+        bgColor: 'gray.50',
+      })}
+    >
+      <div
+        className={css({
+          zIndex: '10',
+          w: 'full',
+          maxW: 'md',
+          overflow: 'hidden',
+          rounded: '2xl',
+          borderWidth: '1px',
+          borderColor: 'gray.100',
+          shadow: 'xl',
+        })}
+      >
+        <div
+          className={css({
+            display: 'flex',
+            flexDir: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+            mt: '3',
+            mb: '3',
+            borderBottomWidth: '1px',
+            borderColor: 'gray.200',
+            bgColor: 'white',
+            pl: '4',
+            pr: '4',
+            pt: '8',
+            pb: '6',
+            textAlign: 'center',
+            sm: { pl: '16', pr: '16' },
+          })}
+        >
           <Link href="/">
             <Image
               src="/logo.png"
               priority
               alt="Logo"
-              className="h-10 w-10 rounded-full"
+              className={css({ h: '10', w: '10', rounded: 'full' })}
               width={20}
               height={20}
             />
           </Link>
-          <h3 className="text-xl font-semibold">Sign Up</h3>
-          <p className="text-sm text-gray-500">
-            Create an account with your email and password
+          <h3 className={css({ fontSize: 'xl', lineHeight: 'xl', fontWeight: 'semibold' })}>Sign In</h3>
+          <p className={css({ fontSize: 'sm', lineHeight: 'sm', color: 'gray.500' })}>
+            Use your email and password to sign in
           </p>
         </div>
         <Form type="register" />
